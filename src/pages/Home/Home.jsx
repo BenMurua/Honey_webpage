1de14121370e651bae6ab2d51be7656511b9c0d0
BenMurua <benat.murua594@comunidadunir.net>
2025-08-07 08:34:57 +0200

    Corregido ruta del video

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import "./Home.css";
 import { useState } from "react";
 import VideoCard from "../../components/VideoCard/VideoCard";
+import VideoPromo from "../../assets/bideo_promo.mp4";
 
 const Home = () => {
   return (
     <div className="home-container">
-      <VideoCard src="src/assets/bideo_promo.mp4"></VideoCard>
+      <VideoCard src={VideoPromo}></VideoCard>
       <p>This is the main content of the home page.</p>
     </div>
   );
