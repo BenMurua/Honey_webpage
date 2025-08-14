ea24d4d43b3f591329c2be369504442abcf85196
BenMurua <benat.murua594@comunidadunir.net>
2025-08-14 14:41:04 +0200

    ContactCard added text in Basque and Spanish

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import ContactCard from "../../components/ContactCard/ContactCard";
 import image_profile from "../../assets/Asier.jpg";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <ContactCard image={image_profile}></ContactCard>
+      <ContactCard
+        image={image_profile}
+        email={"prueba@gmail.com"}
+        phone={"+34 123 456 789"}
+        address={"Calle Falsa 123, Madrid, España"}
+        text={t("Contact.text")}
+      ></ContactCard>
     </div>
   );
 };
