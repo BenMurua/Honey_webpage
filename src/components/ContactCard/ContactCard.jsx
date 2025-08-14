35f9a71afd3ae51b6229431d01da0c5d1e3ae5cd
BenMurua <benat.murua594@comunidadunir.net>
2025-08-14 14:21:20 +0200

    Visual improvement Contact page and ContactCard component

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,12 +1,16 @@
+import "./ContactCard.css";
+
 const ContactCard = ({ image, text, email, phone, address }) => {
   return (
     <div className="contact-card">
-      <img src={image} alt="Contact" className="contact-image" />
+      <div className="contact-image-container">
+        <img src={image} alt="Contact" className="contact-image" />
+      </div>
       <div className="contact-info">
-        <p className="contact-text">{text}</p>
-        <p className="contact-email">Email: {email}</p>
-        <p className="contact-phone">Phone: {phone}</p>
-        <p className="contact-address">Address: {address}</p>
+        {<p className="contact-text">{text}</p>}
+        {<p className="contact-email">✉️ {email}</p>}
+        {<p className="contact-phone">📞 {phone}</p>}
+        {<p className="contact-address">📍 {address}</p>}
       </div>
     </div>
   );
